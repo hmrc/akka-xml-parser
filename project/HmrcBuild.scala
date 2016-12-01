2ae1a4e1ec84722a4aa81303b2d7c81525deec49
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2016-12-01 18:24:30 +0000

    Initial Code

---
+++ project/HmrcBuild.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sbt._
+import sbt.Keys._
+import scoverage.ScoverageKeys
+import uk.gov.hmrc.PublishingSettings._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object HmrcBuild extends Build {
+
+  import uk.gov.hmrc._
+  import DefaultBuildSettings._
+  import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt}
+  import uk.gov.hmrc.PublishingSettings._
+
+  val appName = "akka-xml-parser"
+
+  lazy val microservice = Project(appName, file("."))
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .settings(
+      scalaVersion := "2.11.8",
+      libraryDependencies ++= AppDependencies(),
+      crossScalaVersions := Seq("2.11.8"),
+      resolvers := Seq(
+        Resolver.bintrayRepo("hmrc", "releases"),
+        "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/"
+      ),
+      scoverageSettings
+    )
+
+  lazy val scoverageSettings = {
+    import scoverage.ScoverageSbtPlugin._
+    Seq(
+      // Semicolon-separated list of regexs matching classes to exclude
+      ScoverageKeys.coverageExcludedPackages := "<empty>;.*BuildInfo*.",
+      ScoverageKeys.coverageMinimum := 1,
+      ScoverageKeys.coverageFailOnMinimum := true,
+      ScoverageKeys.coverageHighlighting := true,
+      parallelExecution in Test := false
+    )
+  }
+}
+
+private object AppDependencies {
+
+  import play.sbt.PlayImport._
+  import play.core.PlayVersion
+
+  val compile = Seq(
+    "com.typesafe.play" %% "play" % PlayVersion.current,
+    ws
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test: Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+        "org.scalatest" %% "scalatest" % "2.2.4" % scope,
+        "org.pegdown" % "pegdown" % "1.5.0" % scope
+
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
