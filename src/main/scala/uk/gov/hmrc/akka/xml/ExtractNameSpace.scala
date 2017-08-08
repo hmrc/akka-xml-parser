eac4a2474d1fc87c67b5d25dc1d55f4b0a3818ae
JoemG <joseph.griffiths@digital.hmrc.gov.uk>
2017-08-08 20:38:02 +0100

    refactor for readability

--- src/main/scala/uk/gov/hmrc/akka/xml/ExtractNameSpace.scala
+++ src/main/scala/uk/gov/hmrc/akka/xml/ExtractNameSpace.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.akka.xml
 
 import com.fasterxml.aalto.{AsyncByteArrayFeeder, AsyncXMLStreamReader}
@@ -11,26 +27,29 @@ object ExtractNameSpace {
     val XMLNS = "xmlns"
     val extractedNameSpaces = scala.collection.mutable.Map[String, String]()
 
-    (0 until parser.getNamespaceCount).foreach { i =>
-      val ns = if (parser.getNamespacePrefix(i).length == 0) XMLNS else XMLNS + ":" + parser.getNamespacePrefix(i)
+    (0 until parser.getNamespaceCount).foreach { currentNS =>
+      val ns: String = if (parser.getNamespacePrefix(currentNS).length == 0) XMLNS else XMLNS + ":" + parser.getNamespacePrefix(currentNS)
 
-      targetNameSpace.foreach{
-        case (k,v) =>
-          if (v == parser.getNamespaceURI(i)) {
-            extractedNameSpaces +=(ns -> parser.getNamespaceURI(i))
-          }
-      }
+      compareNameSpaceToTarget(targetNameSpace, currentNS, ns)
     }
 
     (0 until parser.getAttributeCount).foreach { i =>
       if (targetNameSpace.isEmpty) {
-        extractedNameSpaces.+=(parser.getAttributeLocalName(i) -> parser.getAttributeValue(i))
+        extractedNameSpaces += (parser.getAttributeLocalName(i) -> parser.getAttributeValue(i))
       } else if (targetNameSpace.keySet(parser.getAttributeLocalName(i)) || targetNameSpace.keySet(
         parser.getAttributePrefix(i) + ":" + parser.getAttributeLocalName(i))) {
-        extractedNameSpaces.+=(parser.getAttributeLocalName(i) -> parser.getAttributeValue(i))
+        extractedNameSpaces += (parser.getAttributeLocalName(i) -> parser.getAttributeValue(i))
       }
     }
-    extractedNameSpaces
-  }.toMap
 
+    def compareNameSpaceToTarget(targetNameSpace: Map[String, String], i: Int, ns: String) = {
+      targetNameSpace.foreach {
+        case (k, v) =>
+          if (v == parser.getNamespaceURI(i)) {
+            extractedNameSpaces += (ns -> parser.getNamespaceURI(i))
+          }
+      }
+    }
+    extractedNameSpaces.toMap
+  }
 }
