7dcd1a6e40d8123efab7f416c86e862c5d9c81d1
gabor-dorcsinecz <gabor.dorcsinecz@digital.hmrc.gov.uk>
2017-10-23 17:20:12 +0100

    Hotfix for xml prolog parsing

---
+++ src/test/scala/uk/gov/hmrc/akka/xml/XmlEncodingYankerStageSpec.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.akka.xml
+
+import akka.util.ByteString
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+import scala.concurrent.duration._
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+import akka.stream.testkit.scaladsl.TestSink
+import akka.stream.testkit.scaladsl.TestSource
+
+class XmlEncodingYankerStageSpec extends FlatSpec with BeforeAndAfter{
+
+  it should "Work on chopped up xml prlogs" in {
+    val as = ActorSystem("XmlEncodingYankerStage")
+    val am = ActorMaterializer()(as)
+    val source = TestSource.probe[ByteString](as)
+    val sink = TestSink.probe[ByteString](as)
+    val chunk = XmlEncodingYankerStage.parser()
+
+    //val (pub,sub) = source.via(chunk).alsoTo(Sink.foreach(a => println(a.utf8String))).toMat(sink)(Keep.both).run()(am)
+    val (pub,sub) = source.via(chunk).toMat(sink)(Keep.both).run()(am)
+    sub.request(10)
+    pub.sendNext(ByteString("<?xml versi"))
+    sub.expectNoMsg()
+    pub.sendNext(ByteString("""on="1.0" enco"""))
+    sub.expectNoMsg()
+    pub.sendNext(ByteString("""ding="ISO-88"""))
+    sub.expectNoMsg()
+    pub.sendNext(ByteString("""59-1"?><GovTalkMe"""))
+    sub.expectNext(ByteString("""<?xml version="1.0" encoding="UTF-8"?><GovTalkMe"""))
+    //sub.request(1)
+    pub.sendNext(ByteString("""ssage xsi:schemaLocation="http://www.govtalk.gov.uk/CM/envelope"""))
+    sub.expectNext(ByteString("""ssage xsi:schemaLocation="http://www.govtalk.gov.uk/CM/envelope"""))
+    pub.sendComplete()
+  }
+
+  it should "Work on whole xml prologs" in {
+    val as = ActorSystem("XmlEncodingYankerStage")
+    val am = ActorMaterializer()(as)
+    val source = TestSource.probe[ByteString](as)
+    val sink = TestSink.probe[ByteString](as)
+    val chunk = XmlEncodingYankerStage.parser()
+
+    val (pub,sub) = source.via(chunk).toMat(sink)(Keep.both).run()(am)
+    sub.request(10)
+    pub.sendNext(ByteString("""<?xml version="1.0" encoding="ISO-8859-1"?><GovTalkMessage xsi:schemaLocation="http://www.govtalk."""))
+    sub.expectNext(ByteString("""<?xml version="1.0" encoding="UTF-8"?><GovTalkMessage xsi:schemaLocation="http://www.govtalk."""))
+  }
+
+
+  it should "Work on short strings" in {
+    val as = ActorSystem("XmlEncodingYankerStage")
+    val am = ActorMaterializer()(as)
+    val source = TestSource.probe[ByteString](as)
+    val sink = TestSink.probe[ByteString](as)
+    val chunk = XmlEncodingYankerStage.parser()
+
+    val (pub,sub) = source.via(chunk).toMat(sink)(Keep.both).run()(am)
+    sub.request(10)
+    pub.sendNext(ByteString("""<?xml version="1.0" encoding="ISO-88"""))
+    sub.expectNoMsg()
+    pub.sendComplete()
+    sub.expectNext(ByteString("""<?xml version="1.0" encoding="ISO-88"""))
+    sub.expectComplete()
+  }
+
+
+}
